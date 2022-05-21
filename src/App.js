afad979a1244d3b9acfb16bb69df6462008a642f
najmuleimon <najmul.eimon@gmail.com>
2022-05-21 23:49:26 +0600

    navbar routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Pages/Shared/Navbar';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Signup from './Pages/Login/Signup';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import Purchase from './Pages/Purchase/Purchase';
+import Blogs from './Pages/Blogs/Blogs';
+import NotFound from './Pages/NotFound/NotFound';
 
 function App() {
   return (
     <div>
-      <h1>hello tailwind</h1>
-      <button class="btn btn-primary">Button</button>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='login' element={<Login />} />
+        <Route path='signup' element={<Signup />} />
+        <Route path='dashboard' element={<Dashboard />} />
+        <Route path='purchase' element={<Purchase />} />
+        <Route path='blogs' element={<Blogs />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
